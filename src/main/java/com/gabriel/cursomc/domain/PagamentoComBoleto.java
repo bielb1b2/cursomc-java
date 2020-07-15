9877faf71a165dbe7cff10db3af230cfd55bcfd1
bielb1b2 <gabrielvt.souza@hotmail.com>
2020-07-15 10:59:44 -0300

    endpoint /pedidos/{id} funcionando

--- src/main/java/com/gabriel/cursomc/domain/PagamentoComBoleto.java
+++ src/main/java/com/gabriel/cursomc/domain/PagamentoComBoleto.java
@@ -4,20 +4,24 @@ import java.util.Date;
 
 import javax.persistence.Entity;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.gabriel.cursomc.domain.enums.EstadoPagamento;
 
 @Entity
 public class PagamentoComBoleto extends Pagamento {
 	private static final long serialVersionUID = 1L;
 
-
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private Date dataVencimento;
+	
+	@JsonFormat(pattern = "dd/MM/yyyy HH:mm")
 	private Date dataPagamento;
 	
 	public PagamentoComBoleto() {
 		
 	}
 
+	
 	public PagamentoComBoleto(Integer id, EstadoPagamento estado, Pedido pedido, Date dataVencimento, Date dataPagamento) {
 		super(id, estado, pedido);
 		this.dataPagamento = dataPagamento;
