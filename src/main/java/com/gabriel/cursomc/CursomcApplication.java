546932f62631ea20b60eeda22699c61e75d25b04
bielb1b2 <gabrielvt.souza@hotmail.com>
2020-07-15 11:14:10 -0300

    Arrumando os JsonBackReference/JsonManageReference para JsonIgnore e excluido o desnecessario

--- src/main/java/com/gabriel/cursomc/CursomcApplication.java
+++ src/main/java/com/gabriel/cursomc/CursomcApplication.java
@@ -1,6 +1,6 @@
 package com.gabriel.cursomc;
 
-import java.text.SimpleDateFormat;import java.util.ArrayList;
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -64,6 +64,7 @@ public class CursomcApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
+
 
 	@Override
 	public void run(String... args) throws Exception {
