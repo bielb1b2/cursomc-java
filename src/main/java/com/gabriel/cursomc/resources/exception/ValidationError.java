e66072c5baac2ec9ae7752f1a7880cb9be628f88
bielb1b2 <gabrielvt.souza@hotmail.com>
2020-07-16 13:11:02 -0300

    Personalizando erros de validação

---
+++ src/main/java/com/gabriel/cursomc/resources/exception/ValidationError.java
@@ -0,0 +1,27 @@
+package com.gabriel.cursomc.resources.exception;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ValidationError extends StandardError {
+	private static final long serialVersionUID = 1L;
+	
+	private List<FieldMessage> errors = new ArrayList<>();
+	
+	public ValidationError(Integer status, String msg, Long timestamp) {
+		super(status, msg, timestamp);
+		// TODO Auto-generated constructor stub
+	}
+
+	public List<FieldMessage> getErrors() {
+		return errors;
+	}
+
+	public void AddError(String fieldName, String messagem) {
+		errors.add(new FieldMessage(fieldName, messagem));
+	}
+	
+	
+	
+}
